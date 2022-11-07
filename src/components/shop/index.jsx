be6d158ad92cd1174a995b66974d2ec0706485bb
Parker Nix <71665527+ParkerNix@users.noreply.github.com>
2022-11-07 02:54:23 -0500

    structure building

--- src/components/shop/index.jsx
+++ src/components/shop/index.jsx
@@ -1,7 +1,12 @@
 export const Shop = () => {
     return (
         <div>
-            <p>omg the shop</p>
+            <div>
+                <h2>Title</h2>
+                <p>omg the shop</p>
+                <button>ADD TO CART - $<span>4.00</span></button>
+            </div>
+            <img src="../maybe react.png" alt="react"></img>
         </div>
     )
 }
